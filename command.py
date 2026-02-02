883376e5ba07468b9c2b3655f0fdcac5e249a9b9
Hari__Patel <patelhari1324@gmail.com>
2026-02-02 10:34:43 +0530

    Command line argument

---
+++ command.py
@@ -0,0 +1,31 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:58:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #1.int
+>>> a = int(input("Enter number: "))
+Enter number: 45
+>>> print(a)
+45
+>>> 
+>>> #2.float
+>>> b = float(input("Enter decimal number: "))
+Enter decimal number: 560.89
+>>> print(b)
+560.89
+>>> 
+>>> #3.Str
+>>> x=input("Enter Name:")
+Enter Name:Hari
+>>> print(c)
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    print(c)
+NameError: name 'c' is not defined
+>>> print(x)
+Hari
+>>> 
+>>> #4.bool
+>>> d = input("Enter you are student: ")
+Enter you are student: True
+>>> print(d)
+True
+>>> 
