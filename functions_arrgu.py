6a0eee44923d95de1f1a4a4a7eacd03fd9bbebe6
Hari__Patel <patelhari1324@gmail.com>
2026-03-30 10:26:01 +0530

    default and function

---
+++ functions_arrgu.py
@@ -0,0 +1,26 @@
+#1.basic positional argument
+def add(a, b):
+    print("a =", a)
+    print("b =", b)
+    return a + b
+
+result = add(2, 5)
+print("sum =", result)
+
+#2.student iinformation
+def student_index(name,roll,marks):
+    print("name",name)
+    print("rool no",roll)
+    print("marks:",marks)
+ student_index("ravi",101,88) 
+ 
+ #.error with wrong number of argument
+ #1.simple interest
+ def simple_interest(p,r,m)
+    si=(p*r*n)(100):
+        print("Simple intst")
+        simple_interst
+    
+ 
+ 
+ 
